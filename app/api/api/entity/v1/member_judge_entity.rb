3958ffd27b18b961738bbeb1f137ad6a2c2c035f
藤田 晃平 <k.fujita@MAC0065.local>
2020-01-08 11:40:57 +0900

    seventh-commit

--- app/api/api/entity/v1/member_judge_entity.rb
+++ app/api/api/entity/v1/member_judge_entity.rb
@@ -2,7 +2,6 @@
 #   module V1
 #     class MemberJudgeEntity < Grape::Entity
 #       root 'judged_candidates_results'
-#       JudgedCandidatesResules.new
 #       end
 #     end
 #   end
--- app/api/api/judgement/v1/member_judge_api.rb
+++ app/api/api/judgement/v1/member_judge_api.rb
@@ -37,12 +37,13 @@ module API
 
         post '/' do
           member_candidates = params[:member_candidates]
-          def ListResults
-            judeged_candidates_results = []
-          end
+
+          judged_candidates_results = []
+          judged_result = MemberJudgeAPI.new
 
 
           member_candidates.each do |member|
+
 
           member_name =  member[:member_name]
           event_planning = member[:event_planning]
@@ -55,91 +56,70 @@ module API
             # パラメータ必須チェック
           if
             member_name == ""
-            {"member_name": nil,
-             "error_message": "隊員名を設定してください。"
-            }
+            judged_result = {"member_name": nil, "error_message": "隊員名を設定してください。"}
           elsif
             event_planning == nil
-            {"member_name": member_name,
-             "error_message": "イベント企画力を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "イベント企画力を設定してください。"}
           elsif
             cogitation == nil
-            {"member_name": member_name,
-             "error_message": "思考力を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "思考力を設定してください。"}
           elsif
             coordination == nil
-            {"member_name": member_name,
-             "error_message": "調整力を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "調整力を設定してください。"}
           elsif
             programming_ability == nil
-            {"member_name": member_name,
-             "error_message": "プログラム製造力を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "プログラム製造力を設定してください。"}
           elsif
             infrastructure_knowledge == nil
-            {"member_name": member_name,
-             "error_message": "基盤理解を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "基盤理解を設定してください。"}
 
             # パラメータ形式チェック
           elsif
             member_name == "#{member_name.to_i}"
-            {"member_name": nil,
-             "error_message": "隊員名は文字列で入力してください。"
-            }
+            judged_result = {"member_name": nil, "error_message": "隊員名は文字列で入力してください。"}
           elsif
             event_planning < 1 || 5 < event_planning
-            {"member_name": member_name,
-             "error_message": "イベント企画力は1から5までの数値を設定してください。	"
-            }
+            judged_result = {"member_name": member_name, "error_message": "イベント企画力は1から5までの数値を設定してください。	"}
           elsif
             cogitation < 1 || 5 < cogitation
-            {"member_name": member_name,
-             "error_message": "思考力は1から5までの数値を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "思考力は1から5までの数値を設定してください。"}
           elsif
             coordination < 1 || 5 < coordination
-            {"member_name": member_name,
-             "error_message": "調整力は1から5までの数値を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "調整力は1から5までの数値を設定してください。"}
           elsif
             programming_ability < 1 || 5 < programming_ability
-            {"member_name": member_name,
-             "error_message": "プログラム製造力は1から5までの数値を設定してください。"
-            }
+            judged_result = {"member_name": member_name, "error_message": "プログラム製造力は1から5までの数値を設定してください。"}
           elsif
             infrastructure_knowledge < 1 || 5 < infrastructure_knowledge
-            {"member_name": member_name,
-             "error_message": "基盤理解は1から5までの数値を設定してください。																	"
-            }
+            judged_result = {"member_name": member_name, "error_message": "基盤理解は1から5までの数値を設定してください。"}
 
           # 本処理
           elsif
-          event_planning <= 1 || cogitation <= 1 || coordination <= 1 || event_planning + cogitation + coordination + programming_ability + infrastructure_knowledge <= 10
-            {"member_name": member_name,
-             "enlisted_propriety": false
-            }
+            event_planning <= 1 || cogitation <= 1 || coordination <= 1 || event_planning + cogitation + coordination + programming_ability + infrastructure_knowledge <= 10
+            judged_result = {"member_name": member_name, "enlisted_propriety": false}
           else
-            {"member_name": member_name,
-             "enlisted_propriety": true
-            }
+            judged_result = {"member_name": member_name, "enlisted_propriety": true}
+
 
 
           end
+
+
+          judged_candidates_results.push(judged_result)
+
           end
 
-          # resource :judged_candidates_results do
-          #   desc 'GET /api/v1/member_judge_entity'
-          #   get '/' do
-          #   end
-          #   post '/' do
-          #
-          #   end
+          judged_candidates_results
+
+        # resource :judged_candidates_results do
+        #   desc 'GET /api/v1/member_judge_entity'
+        #
+        #   post '/' do
+        #     present judged_candidates_results, with: Entity::V1::MemberJudgeEntity
+        #   end
+        # end
+
         end
-
       end
     end
   end
