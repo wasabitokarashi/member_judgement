f8963cca996bde1696e7affbfb2de53a78eed854
Kohei Fujita <kfujita@netprotections.co.jp>
2020-01-10 15:47:54 +0900

    [fix]条件式の記載を修正
    [mod]

--- app/api/services/memeber_judge_service.rb
+++ app/api/services/memeber_judge_service.rb
@@ -28,50 +28,37 @@ module Services
         # バリデーション
         # パラメータ必須チェック
 
-        if
-        member_name == ""
+        if member_name == ""
           judged_result = {"member_name": nil, "error_message": "隊員名を設定してください。"}
-        elsif
-        event_planning == nil
+        elsif event_planning == nil
           judged_result = {"member_name": member_name, "error_message": "イベント企画力を設定してください。"}
-        elsif
-        cogitation == nil
+        elsif cogitation == nil
           judged_result = {"member_name": member_name, "error_message": "思考力を設定してください。"}
-        elsif
-        coordination == nil
+        elsif coordination == nil
           judged_result = {"member_name": member_name, "error_message": "調整力を設定してください。"}
-        elsif
-        programming_ability == nil
+        elsif programming_ability == nil
           judged_result = {"member_name": member_name, "error_message": "プログラム製造力を設定してください。"}
-        elsif
-        infrastructure_knowledge == nil
+        elsif infrastructure_knowledge == nil
           judged_result = {"member_name": member_name, "error_message": "基盤理解を設定してください。"}
 
           # パラメータ形式チェック
 
-        elsif
-        member_name == "#{member_name.to_i}"
+        elsif member_name == "#{member_name.to_i}"
           judged_result = {"member_name": nil, "error_message": "隊員名は文字列で入力してください。"}
-        elsif
-        event_planning < 1 || 5 < event_planning
+        elsif event_planning < 1 || 5 < event_planning
           judged_result = {"member_name": member_name, "error_message": "イベント企画力は1から5までの数値を設定してください。"}
-        elsif
-        cogitation < 1 || 5 < cogitation
+        elsif cogitation < 1 || 5 < cogitation
           judged_result = {"member_name": member_name, "error_message": "思考力は1から5までの数値を設定してください。"}
-        elsif
-        coordination < 1 || 5 < coordination
+        elsif coordination < 1 || 5 < coordination
           judged_result = {"member_name": member_name, "error_message": "調整力は1から5までの数値を設定してください。"}
-        elsif
-        programming_ability < 1 || 5 < programming_ability
+        elsif programming_ability < 1 || 5 < programming_ability
           judged_result = {"member_name": member_name, "error_message": "プログラム製造力は1から5までの数値を設定してください。"}
-        elsif
-        infrastructure_knowledge < 1 || 5 < infrastructure_knowledge
+        elsif infrastructure_knowledge < 1 || 5 < infrastructure_knowledge
           judged_result = {"member_name": member_name, "error_message": "基盤理解は1から5までの数値を設定してください。"}
 
           # 本処理
 
-        elsif
-        event_planning <= 1 || cogitation <= 1 || coordination <= 1 || programming_ability <= 1 || infrastructure_knowledge <= 1 || skill_sum <= 10
+        elsif event_planning <= 1 || cogitation <= 1 || coordination <= 1 || programming_ability <= 1 || infrastructure_knowledge <= 1 || skill_sum <= 10
           judged_result = {"member_name": member_name, "enlisted_propriety": false}
         else
           judged_result = {"member_name": member_name, "enlisted_propriety": true}
